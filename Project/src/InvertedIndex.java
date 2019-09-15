ad01706b83115b50553718d77bddff2ff70e75b0
Safya <Safya@192.168.0.26>
2019-09-14 23:01:23 -0700

    Added constrcutor

--- Project/src/InvertedIndex.java
+++ Project/src/InvertedIndex.java
@@ -1,3 +1,9 @@
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+
 
 /**
  * @author Safya Osman
@@ -5,5 +11,25 @@
  */
 public class InvertedIndex
 {
+	private final Map<String, TreeMap<String, TreeSet<Integer>>> map;
+	
+	public InvertedIndex()
+	{
+		map = new TreeMap<>();
+	}
+	
+//	TODO Implement getters & setters
+//	public void addKey(String key, TreeMap<String, TreeSet<Integer>> littleMap)
+//	{
+//		
+//	}
+//	public addTreeValue(TreeMap<String, TreeSet<Integer>> value);
+//	public addIntValue(String key, String treeValue, Integer value);
+	
+//	public getKey();
+//	public getValue();
+//	public getValueOfValue();
+//	public toString()
+
 	
 }
