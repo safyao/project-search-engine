554f946da54129f6e82518b6240082045394967c
Safya <Safya@10.1.4.106>
2019-09-16 14:57:32 -0700

    Added complete TODO list.

--- Project/src/Driver.java
+++ Project/src/Driver.java
@@ -1,7 +1,9 @@
+import java.io.IOException;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.TreeMap;
@@ -23,8 +25,9 @@ public class Driver {
 	 * inverted index.
 	 *
 	 * @param args flag/value pairs used to start this program
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// store initial start time
 		Instant start = Instant.now();
 
@@ -32,24 +35,51 @@ public class Driver {
 		System.out.println(Arrays.toString(args));
 		
 		
+		
+		
+		
 		//Take argument - includes file location, output file location, count location
 		ArgParser parsedArgs = new ArgParser();
 		
-		//For path in parsedargs, get file locations
-		//Taverse the directory for all the text files - gives an ArrayList of Paths
-		DirectoryTraverser traverse = new DirectoryTraverser();
-		//for each file in allFiles, parse and stem the words - returns TreeSet of Strings
-		//Create InvertedIndex data structure (similar to ArgParser)
-		//Enter keys into index, with the same file location, and incrementor for line number
+		Path path = parsedArgs.getPath("-path");
 		
-		//Write the index into files as Json object
+		//If directory, traverse the directory for all the text files - gives an ArrayList of Paths
+
+		List<Path> allFiles = DirectoryTraverser.traverseDirectory(path);
+		
+		for (Path file : allFiles)
+		{
+			TreeSet<String> stems = TextStemmer.uniqueStems(file);
+			
+		}
+		//for each path, use uniqueStems(Path) - returns TreeSet of stem Strings from file
+		
+		//Create InvertedIndex data structure (similar to ArgParser)
+		//go through TreeSet and add stems to Index (addKey), the file and initial location (increment for line number) 
+		//(addValue, addSecondValue)
+		//then add second value if present by providing stem and file name
+			//need addKey method (if not already there? Allows duplicates?)
+			//addValue (TreeSet)
+			//addInteger
+		
 		//If index then output file:
+		//Write the index into files as Json object (use asNestedObject for file)
 		//parsedArgs.getPath("-index", Path.of("index.json").normalize());
-		//Somehow figure out the word counts iff
 		//parsedArgs.getPath("-counts", Path.of("counts.json").normalize());
 
+		//if want count, then parseFile() and output size
 		
-
+		//Exceptions
+		//Clean Up
+		//Javadoc
+		
+		
+		
+		
+		
+		
+		
+		
 		// calculate time elapsed and output
 		Duration elapsed = Duration.between(start, Instant.now());
 		double seconds = (double) elapsed.toMillis() / Duration.ofSeconds(1).toMillis();
