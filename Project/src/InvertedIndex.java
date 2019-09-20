1d5af2ead295eaf6ed1992f1705b38c6a492bd31
Safya <Safya@192.168.0.26>
2019-09-20 12:26:39 -0700

    Completely restructured InvertedIndex.

--- Project/src/InvertedIndex.java
+++ Project/src/InvertedIndex.java
@@ -17,76 +17,39 @@ public class InvertedIndex
 		map = new TreeMap<>();
 	}
 	
-	public void addKey(String key, TreeMap<String, TreeSet<Integer>> value)
+	public void addKey(String word, String path, Integer position)
 	{
-		if (!map.containsKey(key))
+		if (!map.containsKey(word))
 		{
-			map.put(key, value);
+			map.put(word, new TreeMap<String, TreeSet<Integer>>());
 		}
-		else
+		if (!map.get(word).containsKey(path))
 		{
-			map.replace(key, value);
+			map.get(word).put(path, new TreeSet<Integer>());
+		}
+		if (!map.get(word).get(path).contains(position))
+		{
+			map.get(word).get(path).add(position);
 		}
 	}
 	
-	public TreeMap<String, TreeSet<Integer>> getValue(String key)
-	{
-		return map.get(key);
-	}
-	
+//	public TreeMap<String, TreeSet<Integer>> getValue(String key)
+//	{
+//		return map.get(key);
+//	}
+//	
 	public Map<String, TreeMap<String, TreeSet<Integer>>> getIndex()
 	{
 		return map;
 	}
-	
-	public Set<String> getKeys()
-	{
-		return map.keySet();
-	}
+//	
+//	public Set<String> getKeys()
+//	{
+//		return map.keySet();
+//	}
 	
 	public String toString()
 	{
 		return map.toString();
 	}
-	
-//	public void addStringValue(String key, String value, TreeSet<Integer> setValue)
-//	{
-//		if (map.containsKey(key))
-//		{
-//			TreeMap<String, TreeSet<Integer>> newValue = map.get(key);
-//			if (newValue.containsKey(value))
-//			{
-//				newValue.replace(value, setValue);
-//			}
-//			else
-//			{
-//				
-//			}
-//			
-//			map.replace(key, newValue);
-//		}
-//	}
-//	public void addIntValue(String key, String value, Integer intValue)
-//	{
-//		if (map.containsKey(key))
-//		{
-//			TreeSet<Integer> newValue = map.get(key).get(value);	//sets dont allow duplicates anyway
-//			newValue.add(intValue);
-//
-//		}
-//	}
-	
-	
-//	TODO Implement getters & setters
-
-//	public addValue(TreeMap<String, TreeSet<Integer>> value);
-//	public addIntValue(String key, String treeValue, Integer value);
-	
-//	public getElement();
-//	public getKey();
-//	public getValue();
-//	public getIntValue();
-//	public toString()
-
-	
 }
