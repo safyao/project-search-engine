c352e01ead2cfdb1999e2392bc85fceb51fd993b
Safya <Safya@192.168.0.26>
2019-09-20 19:43:30 -0700

    Added Javadoc comments.

--- Project/src/InvertedIndex.java
+++ Project/src/InvertedIndex.java
@@ -1,22 +1,38 @@
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.TreeSet;
-import java.util.Set;
 
 
 /**
- * @author Safya Osman
+ * Nested data structure class that houses Integers, TreeSets, Strings, and TreeMaps in a Map.
  *
+ * @author CS 212 Software Development
+ * @author University of San Francisco
+ * @version Fall 2019
  */
 public class InvertedIndex
 {
+	/**
+	 * Stores arguments in key = value pairs.
+	 */
 	private final Map<String, TreeMap<String, TreeSet<Integer>>> map;
 	
+	/**
+	 * Initializes this argument map.
+	 */
 	public InvertedIndex()
 	{
 		map = new TreeMap<>();
 	}
 	
+	/**
+	 * Adds elements to map if the given element does not already exist for the corresponding 
+	 * key(s). If a new word or path is being entered, also initializes it's associated value.
+	 * 
+	 * @param word the word to add if it's not already in the map
+	 * @param path the path to add if it's not already mapped to the corresponding word
+	 * @param position the position to add if it's not already mapped to the corresponding word and path
+	 */
 	public void add(String word, String path, Integer position)
 	{
 		if (!map.containsKey(word))
@@ -33,12 +49,17 @@ public class InvertedIndex
 		}
 	}
 
+	/**
+	 * Returns the argument map.
+	 * 
+	 * @return the map
+	 */
 	public Map<String, TreeMap<String, TreeSet<Integer>>> getIndex()
 	{
 		return map;
 	}
 
-	
+	@Override
 	public String toString()
 	{
 		return map.toString();
