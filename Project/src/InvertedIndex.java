2c2ad92469ed38e7e152274b9e8e0dca2fcdcc0c
Safya <Safya@192.168.0.26>
2019-10-05 11:59:33 -0700

    Added writeIndex() method

--- Project/src/InvertedIndex.java
+++ Project/src/InvertedIndex.java
@@ -1,5 +1,5 @@
-import java.util.Collections;
-import java.util.Map;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
@@ -15,7 +15,7 @@ public class InvertedIndex {
 	/**
 	 * Stores arguments in key = value pairs.
 	 */
-	private final Map<String, TreeMap<String, TreeSet<Integer>>> map; // TODO TreeMap<String, TreeMap<String, TreeSet<Integer>>> map; 
+	private final TreeMap<String, TreeMap<String, TreeSet<Integer>>> map;
 	
 	/**
 	 * Initializes this argument map.
@@ -33,43 +33,22 @@ public class InvertedIndex {
 	 * @param position the position to add if it's not already mapped to the corresponding word and path
 	 */
 	public void add(String word, String path, Integer position) {
-		if (!map.containsKey(word)) {
-			map.put(word, new TreeMap<String, TreeSet<Integer>>());
-		}
-		if (!map.get(word).containsKey(path)) {
-			map.get(word).put(path, new TreeSet<Integer>());
-		}
-		if (!map.get(word).get(path).contains(position)) {
-			map.get(word).get(path).add(position);
-		}
-		
-		/* TODO
 		map.putIfAbsent(word, new TreeMap<String, TreeSet<Integer>>());
 		map.get(word).putIfAbsent(path, new TreeSet<Integer>());
 		map.get(word).get(path).add(position);
-		*/
 	}
 
 	// TODO Still breaks encapsulation... see: 
 	// https://github.com/usf-cs212-fall2019/lectures/blob/master/OOP%20Principles/src/PrefixMap.java#L222-L230
 	// https://github.com/usf-cs212-fall2019/lectures/blob/master/OOP%20Principles/src/PrefixDemo.java#L72-L78
-	/**
-	 * Returns an unmodifiable view of the argument map.
-	 * 
-	 * @return the map
-	 */
-	public Map<String, TreeMap<String, TreeSet<Integer>>> getIndex() { // TODO Remove
-		return Collections.unmodifiableMap(map);
-	}
 
 	@Override
 	public String toString() {
 		return map.toString();
 	}
-	
-	/* TODO
+
 	public void writeIndex(Path path) throws IOException {
 		JsonWriter.asDoubleObject(map, path);
 	}
-	*/
+
 }
