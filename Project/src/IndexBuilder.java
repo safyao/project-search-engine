d722df5b1fd84970b32090e607a769b74ac7784a
Safya <Safya@192.168.0.26>
2019-10-06 11:51:50 -0700

    Removed call to uniqueStems().

--- Project/src/IndexBuilder.java
+++ Project/src/IndexBuilder.java
@@ -5,34 +5,39 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
+import opennlp.tools.stemmer.snowball.SnowballStemmer;
+
 
 /**
  * Utility class that stores path data into Inverted Index data structure.
- * 
+ *
  * @author CS 212 Software Development
  * @author University of San Francisco
  * @version Fall 2019
  */
 public class IndexBuilder {
-	
+
+	/** The default stemmer algorithm used by this class. */
+	public static final SnowballStemmer.ALGORITHM DEFAULT = SnowballStemmer.ALGORITHM.ENGLISH;
+
 	/**
 	 * Traverses directory and adds each text file to index.
-	 * 
+	 *
 	 * @param path the path to traverse
 	 * @param index the index to store data into
 	 * @throws IOException if unable to access file
 	 */
 	public static void buildIndex (Path path, InvertedIndex index) throws IOException {
 		List<Path> files = DirectoryTraverser.traverseDirectory(path);
-		
+
 		for (Path item : files) {
 			addPath (item, index);
 		}
 	}
-	
+
 	/**
 	 * Adds path element into Inverted Index data structure.
-	 * 
+	 *
 	 * @param path the path to gather data from for index
 	 * @param index the index to store data into
 	 * @throws IOException if unable to access path
@@ -40,28 +45,29 @@ public class IndexBuilder {
 	public static void addPath (Path path, InvertedIndex index) throws IOException {
 		int wordCount = 0;
 		int positionCount = 0;
-		
+
 		// Reads text file line by line.
 		try (
 				BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
 		) {
 			String line = null;
-			
+
 			while ((line = reader.readLine()) != null) {
-				
+
 				// Parses line and updates word count of text file.
 				String[] parsedLine = TextParser.parse(line);
 				wordCount = wordCount + parsedLine.length;
-				
-				// Stems line and adds each word to index with associated position.
-				List<String> stemmedLine = TextStemmer.uniqueStems(line);
-				for (String item : stemmedLine) {
-					index.add(item, path.toString(), ++positionCount);
+
+				// Stems word and adds each word to index with associated position.
+				for (String word : parsedLine)
+				{
+					String stemmedWord = (String)new SnowballStemmer(DEFAULT).stem(word);
+					index.add(stemmedWord, path.toString(), ++positionCount);
 				}
 			}
 			// Adds word count to index's word count map.
 			index.addCount(path.toString(), Integer.valueOf(wordCount));
 		}
-		
+
 	}
 }
