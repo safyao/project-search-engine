111c6c13b946fe0f2ff904854d57e68e860a6012
Safya <Safya@192.168.0.26>
2019-10-08 16:18:50 -0700

    Fixed word counting method.

--- Project/src/InvertedIndex.java
+++ Project/src/InvertedIndex.java
@@ -5,23 +5,23 @@ import java.util.TreeSet;
 
 
 /**
- * Nested data structure class that houses Strings, TreeMaps, TreeSets, and Integers in one Map 
+ * Nested data structure class that houses Strings, TreeMaps, TreeSets, and Integers in one Map
  * and Strings and Integers in another.
- * 
+ *
  *
  * @author CS 212 Software Development
  * @author University of San Francisco
  * @version Fall 2019
  */
 public class InvertedIndex {
-	
+
 	/** Stores arguments in key = value pairs. **/
 	private final TreeMap<String, TreeMap<String, TreeSet<Integer>>> map;
-	
+
 	/** Stores arguments in key = value pairs. **/
 	private final TreeMap<String, Integer> countsMap;
-	
-	
+
+
 	/**
 	 * Initializes the argument maps.
 	 */
@@ -29,11 +29,11 @@ public class InvertedIndex {
 		map = new TreeMap<>();
 		countsMap = new TreeMap<>();
 	}
-	
+
 	/**
-	 * Adds elements to map if the given element does not already exist for the corresponding 
+	 * Adds elements to map if the given element does not already exist for the corresponding
 	 * key(s). If a new word or path is being entered, also initializes it's associated value.
-	 * 
+	 *
 	 * @param word the word to add if it's not already in the map
 	 * @param path the path to add if it's not already mapped to the corresponding word
 	 * @param position the position to add if it's not already mapped to the corresponding word and path
@@ -41,53 +41,33 @@ public class InvertedIndex {
 	public void add(String word, String path, Integer position) {
 		map.putIfAbsent(word, new TreeMap<String, TreeSet<Integer>>());
 		map.get(word).putIfAbsent(path, new TreeSet<Integer>());
-		map.get(word).get(path).add(position);
-		
-		/*
-		 * TODO Calculate word count here.
-		 * 
-		 * 1) If this wasn't a duplicate (check the result of add(position), add 1 to the word count
-		 * 
-		 * 2) Always use the max position as a proxy for the word count
-		 * added position 12 for hello.txt, know we have at least 12 words in hello.txt.
-		 * added position 4 for hello.txt, we do nothing
-		 */
-	}
-	
-	// TODO Remove
-	/**
-	 * Adds String element and its corresponding Integer element to countsMap.
-	 * 
-	 * @param path the path to add to map
-	 * @param count the count to add to corresponding path in map
-	 */
-	public void addCount(String path, Integer count) {
-		if (count > 0) {
+		if (map.get(word).get(path).add(position))
+		{
+			int count = 1 + countsMap.getOrDefault(path, 0);
 			countsMap.put(path, count);
 		}
 	}
-	
-	
+
 	/**
 	 * Writes index in pretty Json format.
-	 * 
+	 *
 	 * @param path the path to write the index to
 	 * @throws IOException if unable to access path
 	 */
 	public void writeIndex(Path path) throws IOException {
 		JsonWriter.asDoubleObject(map, path);
 	}
-	
+
 	/**
 	 * Writes word counts in pretty Json format.
-	 * 
+	 *
 	 * @param path the path to write counts to
 	 * @throws IOException if unable to access path
 	 */
 	public void writeCounts(Path path) throws IOException {
 		JsonWriter.asObject(countsMap, path);
 	}
-	
+
 	@Override
 	public String toString() {
 		return map.toString();
@@ -95,7 +75,7 @@ public class InvertedIndex {
 
 	/* TODO
 	 * Add some contains/getter methods for both data structures (map and countsMap)
-	 * 
+	 *
 	 * public Set<String> getLocations() {
 	 * 		unmodifiable view of the countsMap keyset
 	 * }
@@ -103,10 +83,10 @@ public class InvertedIndex {
 	 * public Set<String> getLocations(String word) {
 	 * 	unmodifiable view of the inner treemap for the word in your map object
 	 * }
-	 * 
+	 *
 	 * public boolean contains(String word)
 	 * public boolean contains(String word, String location)
 	 * public boolean contains(String word, String location, int position)
-	 * 
+	 *
 	 */
 }
