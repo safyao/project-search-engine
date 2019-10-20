903477196efd207ae7b4b3049024db2d04160429
Safya <Safya@192.168.0.26>
2019-10-20 00:40:25 -0700

    Added Javadoc comment.

--- Project/src/SearchResult.java
+++ Project/src/SearchResult.java
@@ -45,10 +45,14 @@ public class SearchResult implements Comparable<SearchResult> {
 	}
 
 	/**
-	 * Anonymous inner class that compares two SearchResults.
+	 * Anonymous inner class that initializes Comparator.
 	 */
 	public static Comparator<SearchResult> SearchComparator = new Comparator<SearchResult>() {
-
+		/**
+		 * Compares two SearchResults.
+		 *
+		 * @return a negative int, zero, or a positive int as "one" is less than, equal to, or greater than "two"
+		 */
 		@Override
 		public int compare(SearchResult one, SearchResult two) {
 			return two.compareTo(one);
