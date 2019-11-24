17144cd97dfd2372d5840cdb2608f8dde0d178d0
Safya <Safya@localhost>
2019-11-23 23:35:27 -0800

    Adding homework classes.

---
+++ Project/src/HtmlFetcher.java
@@ -0,0 +1,176 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * A specialized version of {@link HttpsFetcher} that follows redirects and
+ * returns HTML content if possible.
+ *
+ * @see HttpsFetcher
+ */
+public class HtmlFetcher {
+
+	/**
+	 * Returns {@code true} if and only if there is a "Content-Type" header and the
+	 * first value of that header starts with the value "text/html" (case-insensitive).
+	 *
+	 * @param headers the HTTP/1.1 headers to parse
+	 * @return {@code true} if the headers indicate the content type is HTML
+	 */
+	public static boolean isHtml(Map<String, List<String>> headers) {
+		Pattern patternHead = Pattern.compile("(?i)(Content-Type)");
+		Pattern patternHTML = Pattern.compile("(?i)(text\\/html)");
+
+		for (String item: headers.keySet())
+		{
+			if (item != null)
+			{
+				Matcher matcherHead = patternHead.matcher(item);
+				if (matcherHead.find())
+				{
+					Matcher matcherHTML = patternHTML.matcher(headers.get(item).get(0));
+					if (matcherHTML.find())
+					{
+						return true;
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Parses the HTTP status code from the provided HTTP headers, assuming the
+	 * status line is stored under the {@code null} key.
+	 *
+	 * @param headers the HTTP/1.1 headers to parse
+	 * @return the HTTP status code or -1 if unable to parse for any reasons
+	 */
+	public static int getStatusCode(Map<String, List<String>> headers) {
+		Pattern pattern = Pattern.compile("(?i)(HTTP\\/1.1).(\\d*)");
+
+		for (String item: headers.keySet())
+		{
+			if (item == null)
+			{
+				Matcher matcher = pattern.matcher(headers.get(item).get(0));
+				if (matcher.find())
+				{
+					return Integer.parseInt(matcher.group(2));
+				}
+			}
+		}
+
+		return -1;
+	}
+
+	/**
+	 * Returns {@code true} if and only if the HTTP status code is between 300 and
+	 * 399 (inclusive) and there is a "Location" header with at least one value.
+	 *
+	 * @param headers the HTTP/1.1 headers to parse
+	 * @return {@code true} if the headers indicate the content type is HTML
+	 */
+	public static boolean isRedirect(Map<String, List<String>> headers) {
+		if ((getStatusCode(headers) >= 300) && (getStatusCode(headers) < 400)) {
+			if (headers.containsKey("Location") && !headers.get("Location").isEmpty()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Fetches the resource at the URL using HTTP/1.1 and sockets. If the status
+	 * code is 200 and the content type is HTML, returns the HTML as a single
+	 * string. If the status code is a valid redirect, will follow that redirect if
+	 * the number of redirects is greater than 0. Otherwise, returns {@code null}.
+	 *
+	 * @param url       the url to fetch
+	 * @param redirects the number of times to follow redirects
+	 * @return the html or {@code null} if unable to fetch the resource or the
+	 *         resource is not html
+	 *
+	 * @see HttpsFetcher#openConnection(URL)
+	 * @see HttpsFetcher#printGetRequest(PrintWriter, URL)
+	 * @see HttpsFetcher#getHeaderFields(BufferedReader)
+	 * @see HttpsFetcher#getContent(BufferedReader)
+	 *
+	 * @see String#join(CharSequence, CharSequence...)
+	 *
+	 * @see #isHtml(Map)
+	 * @see #isRedirect(Map)
+	 */
+	public static String fetch(URL url, int redirects) {
+		try {
+			Map<String, List<String>> headers = HttpsFetcher.fetch(url);
+			if (isRedirect(headers) && redirects > 0)
+			{
+				String redirect = headers.get("Location").get(0);
+				return fetch(redirect, redirects - 1);
+			}
+			else if (getStatusCode(headers) == 200 && isHtml(headers))
+			{
+				String lines = String.join("\n", headers.get("Content"));
+				return lines;
+			}
+		}
+		catch (IOException e)
+		{
+			System.err.println("Unable to fetch resource at the URL: " + url.toString());
+		}
+		return null;
+	}
+
+	/**
+	 * Converts the {@link String} url into a {@link URL} object and then calls
+	 * {@link #fetch(URL, int)}.
+	 *
+	 * @param url       the url to fetch
+	 * @param redirects the number of times to follow redirects
+	 * @return the html or {@code null} if unable to fetch the resource or the
+	 *         resource is not html
+	 *
+	 * @see #fetch(URL, int)
+	 */
+	public static String fetch(String url, int redirects) {
+		try {
+			return fetch(new URL(url), redirects);
+		}
+		catch (MalformedURLException e) {
+			return null;
+		}
+	}
+
+	/**
+	 * Converts the {@link String} url into a {@link URL} object and then calls
+	 * {@link #fetch(URL, int)} with 0 redirects.
+	 *
+	 * @param url the url to fetch
+	 * @return the html or {@code null} if unable to fetch the resource or the
+	 *         resource is not html
+	 *
+	 * @see #fetch(URL, int)
+	 */
+	public static String fetch(String url) {
+		return fetch(url, 0);
+	}
+
+	/**
+	 * Calls {@link #fetch(URL, int)} with 0 redirects.
+	 *
+	 * @param url the url to fetch
+	 * @return the html or {@code null} if unable to fetch the resource or the
+	 *         resource is not html
+	 */
+	public static String fetch(URL url) {
+		return fetch(url, 0);
+	}
+}
